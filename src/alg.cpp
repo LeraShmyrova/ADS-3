6e9ae7114bab37e0339c401a5577833f47bc5b0a
LeraShmyrova <94710005+LeraShmyrova@users.noreply.github.com>
2022-03-09 19:39:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,14 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
+  int kol = 0;
+  for (int i = 0; i <= size; i++){
+  if (*(arr + i) == value){  
+  kol += 1;  
+  }
+  if (kol > 0){
+  return kol; 
+  }else{
   return 0; // если ничего не найдено
+  }  
 }
