d18550d1718989cfe00385e47c75103e2f26d330
LeraShmyrova <94710005+LeraShmyrova@users.noreply.github.com>
2022-03-09 20:57:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,12 @@
 int cbinsearch(int *arr, int size, int value) {
   int kol = 0;
   for (int i = 0; i <= size; i++){
-  if (*(arr + i) == value){  
-  kol += 1;  
+  if (*(arr + i) == value){
+  kol += 1;
   }
   if (kol > 0){
-  return kol; 
+  return kol;
   }else{
   return 0; // если ничего не найдено
-  }  
+  }
 }
